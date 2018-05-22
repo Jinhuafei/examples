405fbb7b7e2c46a263098ba36560ff9269400bc1
Matt Fei <mattfei@microsoft.com>
2018-05-21 17:07:59 -0700

    register types in Unity container

---
+++ DependencyInjection/PopularMovies/PopularMovies/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNet.WebFormsDependencyInjection.Unity;
+using PopularMovies.Bizlogic;
+using PopularMovies.Repository;
+using Unity;
+
+namespace PopularMovies
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            var container = this.AddUnity();
+
+            container.RegisterType<IPopularMovie, MovieManager>();
+            container.RegisterType<IMovieRepository, XmlMovieRepository>();
+        }
+    }
+}
